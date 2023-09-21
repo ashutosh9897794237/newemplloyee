d173c9a294ff40b93f0b959c95c5a87cd7c366dd
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-09-21 21:04:12 +0530

    sdfg

--- script.js
+++ script.js
@@ -2,15 +2,32 @@ const form = document.getElementById("form");
 
 const tbody = document.getElementById("tbody");
 
+const employees  = [];
+
   function addEmpolyee(employee){
+
+    for(let i=0; i<employees.length; i++){
+      let e = employees [i];
+     if(e.email === employee.email) {
+      alert("email already exist");
+      return ;
+     }  else if(e.empId === employee.empId) {
+      alert("Employee Id already exist");
+      return ;
+     }
+    }
+
     const tr = document.createElement("tr");
     tr.innerHTML = `<td>${employee.name}</td> 
     <td>${employee.email}</td>
     <td>${employee.empId}</td>
     <td>${employee.company}</td>
     <td>${employee.designation}</td>
+    <td> 
+    //<button onclick="deleteEmployee(this)" data-empId="${employee.empId}"> Delete </button>  </td>
     `;
     tbody.appendChild(tr);
+    employees.push(employee);
 }
 
 
